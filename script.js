9f785c41f19ce8bf0398b2d82c0ae1893f2e1528
Hadeed Abbas <hadeed2k20@gmail.com>
2024-09-13 16:52:20 +0500

    Made UI more attractive and updated JS code

--- script.js
+++ script.js
@@ -1,165 +1,97 @@
 // Html Element Selection
-let numbersOps = document.querySelector(".numbers-operators");
-let container = document.querySelector(".container");
-let cross = document.querySelector(".cross");
-let sign = document.querySelector(".plus-minus");
-let eraseAll = document.querySelector(".eraseAll");
-let percentage = document.querySelector(".percentage");
+const numbersOps = document.querySelector(".numbers-operators");
+const container = document.querySelector(".container");
+const cross = document.querySelector(".cross");
+const sign = document.querySelector(".plus-minus");
+const eraseAll = document.querySelector(".eraseAll");
+const percentage = document.querySelector(".percentage");
 
 // Global Variables
 let leftOperand = "";
 let operator = "";
 let rightOperand = "";
-let result = ``;
-const numberarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const operatorarray = ['+', '-', '*', '/'];
+let result = "";
+const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const operatorArray = ['+', '-', '*', '/'];
 
-// Functions to perform Calculations
-function performCalculation(leftOp, op, rightOp) {
-    if (leftOp === "" || rightOp === "" || op === "") {
+//Functions
+const performCalculation = (leftOp, op, rightOp) => {
+    if (!leftOp || !rightOp || !op) {
         alert("Cannot Perform Calculation yet.");
         return false;
     }
-    let left = parseInt(leftOp);
-    let right = parseInt(rightOp);
-    let result = "";
+    const left = parseInt(leftOp);
+    const right = parseInt(rightOp);
     switch (op) {
-        case '+':
-            result = performAddition(left, right);
-            break;
-        case '-':
-            result = performSubtraction(left, right);
-            break;
-        case '*':
-            result = performMultiplication(left, right);
-            break;
-        case '/':
-            result = performDivision(left, right);
-            break;
-        default:
-            break;
+        case '+': return left + right;
+        case '-': return left - right;
+        case '*': return left * right;
+        case '/': return right === 0 ? true : left / right;
+        default: return;
     }
-    return result;
-}
-
-const performAddition = (left, right) => {
-    return left + right;
 };
 
-const performSubtraction = (left, right) => {
-    return left - right;
+const updateDisplay = (value) => {
+    container.innerText = value || "0";
 };
 
-const performMultiplication = (left, right) => {
-    return left * right;
-}
-
-const performDivision = (left, right) => {
-    if (right === 0) {
-        return true;
-    }
-    return left / right;
+const operate = () => {
+    result = performCalculation(leftOperand, operator, rightOperand);
+    if (result === false) return;
+    if (result === true) return; 
+    leftOperand = result.toString();
+    rightOperand = "";
+    operator = "";
+    updateDisplay(result);
 };
 
 // Event Listeners
 numbersOps.addEventListener('click', (event) => {
-    let target = event.target;
-
-
-    numberarray.forEach(num => {
-        if (num == target.innerText) {
-            if (operator === "" && rightOperand === "") {
-                leftOperand = `${leftOperand + target.innerText}`;
-                container.innerText = leftOperand;
-            }
-            else {
-                rightOperand = `${rightOperand + target.innerText}`;
-                container.innerText = rightOperand;
-            }
+    const target = event.target;
+    if (numberArray.includes(Number(target.innerText))) {
+        if (!operator) {
+            leftOperand += target.innerText;
+            updateDisplay(leftOperand);
+        } else {
+            rightOperand += target.innerText;
+            updateDisplay(rightOperand);
         }
-    });
-
-    operatorarray.forEach(op => {
-        if (op == target.innerText) {
-            operator = `${target.innerText}`;
-        }
-    })
-
-    if (target.innerText === "=") {
-
-        result = performCalculation(leftOperand, operator, rightOperand);
-        if (result === false) {
-            return;
-        }
-        else if (result === true) {
-            return;
-        }
-        leftOperand = result.toString();
-        rightOperand = "";
-        operator = "";
-        container.innerText = result;
+    } else if (operatorArray.includes(target.innerText)) {
+        if (operator && rightOperand) operate();
+        operator = target.innerText;
+    } else if (target.innerText === "=") {
+        operate();
     }
 });
 
 eraseAll.addEventListener('click', () => {
     leftOperand = rightOperand = operator = "";
-    container.innerText = "0";
+    updateDisplay("0");
 });
 
 sign.addEventListener('click', () => {
-    if (rightOperand === "" && operator === "") {
-        let sign = "-";
-        if (leftOperand[0] === "-") {
-            leftOperand[0] = "0";
-        }
-        else {
-            newLeft = sign + leftOperand;
-            leftOperand = newLeft;
-        }
-        container.innerText = leftOperand;
+    if (!operator) {
+        leftOperand = leftOperand.startsWith("-") ? leftOperand.slice(1) : `-${leftOperand}`;
+        updateDisplay(leftOperand);
+    } else {
+        operator = operator === "+" ? "-" : "+";
     }
-    else if (operator === "+") {
-        operator = "-";
-    }
-    else {
-        let sign = "-";
-        if (rightOperand[0] === "-") {
-            rightOperand[0] = "0";
-        }
-        else {
-            newright = sign + rightOperand;
-            rightOperand = newright;
-        }
-        container.innerText = rightOperand;
+    if (rightOperand) {
+        rightOperand = rightOperand.startsWith("-") ? rightOperand.slice(1) : `-${rightOperand}`;
+        updateDisplay(rightOperand);
     }
 });
 
 cross.addEventListener('click', () => {
-    if (rightOperand === "") {
-        let revisedLeftOperand = "";
-
-        for (let i = 0; i < leftOperand.length - 1; i++) {
-            revisedLeftOperand += leftOperand[i];
-        }
-
-        leftOperand = revisedLeftOperand;
-        container.innerText = leftOperand;
+    if (!rightOperand) {
+        leftOperand = leftOperand.slice(0, -1);
+        updateDisplay(leftOperand);
+    } else if (leftOperand === result) {
+        updateDisplay(result);
+    } else {
+        rightOperand = rightOperand.slice(0, -1);
+        updateDisplay(rightOperand);
     }
-    else if (leftOperand === result) {
-        document.innerText = result;
-    }
-    else {
-        let revisedRightOperand = "";
-
-        for (let i = 0; i < rightOperand.length - 1; i++) {
-            revisedRightOperand += rightOperand[i];
-        }
-
-        rightOperand = revisedRightOperand;
-        container.innerText = rightOperand;
-    }
-
-
 });
 
 percentage.addEventListener('click', () => {
@@ -167,5 +99,5 @@ percentage.addEventListener('click', () => {
     leftOperand = result.toString();
     rightOperand = "";
     operator = "";
-    container.innerText = result;
+    updateDisplay(result);
 });
